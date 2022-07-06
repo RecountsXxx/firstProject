15f50eb913839991a5671a58874cbc80f55e23f2
RecountsXxx <107986811+RecountsXxx@users.noreply.github.com>
2022-07-06 20:03:58 +0300

    New calculator TEST
    New

---
+++ phone calculator/phone calculator/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace phone_calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+            richTextBox1.Text = "";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            richTextBox1.Text = richTextBox1.Text + B.Text;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            richTextBox1.Text = richTextBox1.Text + B.Text;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            richTextBox1.Text = richTextBox1.Text + B.Text;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            richTextBox1.Text = richTextBox1.Text + B.Text;
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            richTextBox1.Text = richTextBox1.Text + B.Text;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            richTextBox1.Text = richTextBox1.Text + B.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            richTextBox1.Text = richTextBox1.Text + B.Text;
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            richTextBox1.Text = richTextBox1.Text + B.Text;
+        }
+
+        private void richTextBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = richTextBox1.Text + "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            richTextBox1.Text = richTextBox1.Text + B.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            richTextBox1.Text = richTextBox1.Text + B.Text;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            richTextBox1.Text = richTextBox1.Text + B.Text;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            richTextBox1.Text = richTextBox1.Text + B.Text;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            richTextBox1.Text = richTextBox1.Text + B.Text;
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            richTextBox1.Text = richTextBox1.Text + B.Text;
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            richTextBox1.Text = richTextBox1.Text + B.Text;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            richTextBox1.Text = richTextBox1.Text + B.Text;
+        }
+    }
+}
